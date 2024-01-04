8b75e07a5418d4d17a3bc3265ad193e10d95e488
Mihai George Sturza <mihai.sturza@nevexo.ro>
2024-01-04 22:04:32 +0200

    Small refactoring for DownloadTool

--- scripts/svgboard/tools/download.js
+++ scripts/svgboard/tools/download.js
@@ -2,7 +2,9 @@ import Tool from "./tool.js";
 
 export default class DownloadTool extends Tool {
   constructor(svgBoard) {
-    super(svgBoard, "Download", "images/download.svg");
+    super(svgBoard, "Download", "media/download.svg");
+
+    window.downloadPNG = () => this.downloadPNG();
   }
 
   createButton() {
@@ -17,11 +19,11 @@ export default class DownloadTool extends Tool {
     optionIsland.classList.add("option-island");
 
     const pngButton = document.createElement("button");
-    pngButton.innerHTML = `<img src="images/image.svg" alt="PNG" height="25" width="25"/> `;
+    pngButton.innerHTML = `<img src="media/image.svg" alt="PNG" height="25" width="25"/> `;
     pngButton.addEventListener("click", () => this.downloadPNG());
 
     const svgButton = document.createElement("button");
-    svgButton.innerHTML = `<img src="images/path.svg" alt="SVG" height="25" width="25"/> `;
+    svgButton.innerHTML = `<img src="media/path.svg" alt="SVG" height="25" width="25"/> `;
     svgButton.addEventListener("click", () => this.downloadSVG());
 
     optionIsland.appendChild(pngButton);
@@ -33,38 +35,76 @@ export default class DownloadTool extends Tool {
     document.body.appendChild(container);
   }
 
+  getInlineStyles(element) {
+    const computedStyle = getComputedStyle(element);
+    let inlineStyle = "";
+
+    for (let i = 0; i < computedStyle.length; i++) {
+      const property = computedStyle[i];
+      inlineStyle += `${property}:${computedStyle.getPropertyValue(property)};`;
+    }
+
+    return inlineStyle;
+  }
+
   downloadPNG() {
+    const targetWidth =
+      this.svgBoard.container.viewBox.baseVal.width ||
+      this.svgBoard.container.clientWidth;
+    const targetHeight =
+      this.svgBoard.container.viewBox.baseVal.height ||
+      this.svgBoard.container.clientHeight;
+
+    document.body.setAttribute("style", this.getInlineStyles(document.body));
+    this.svgBoard.container.setAttribute(
+      "style",
+      this.getInlineStyles(this.svgBoard.container)
+    );
+
     const data = new XMLSerializer().serializeToString(this.svgBoard.container);
     const blob = new Blob([data], {
-        type: "image/svg+xml;charset=utf-8"
+      type: "image/svg+xml;charset=utf-8",
     });
+
     const url = URL.createObjectURL(blob);
 
-    const image = new Image();
-    image.width = this.svgBoard.container.width.baseVal.value;
-    image.height = this.svgBoard.container.height.baseVal.value;
+    const image = new Image(targetWidth, targetHeight);
+
+    image.onload = () => {
+      const canvas = document.createElement("canvas");
+      canvas.width = targetWidth;
+      canvas.height = targetHeight;
+
+      const context = canvas.getContext("2d");
+
+      context.drawImage(image, 0, 0);
+      URL.revokeObjectURL(url);
+
+      const dataURL = canvas.toDataURL();
+      this.downloadResource("image.png", dataURL);
+    };
+
     image.src = url;
-    image.addEventListener("load", () => {
-        const canvas = document.createElement("canvas");
-        canvas.width = image.width;
-        canvas.height = image.height;
 
-        const context = canvas.getContext("2d");
-        context.drawImage(image, 0, 0);
-        URL.revokeObjectURL(url);
-
-        const downloadLink = document.createElement("a");
-        downloadLink.download = "image.png";
-        downloadLink.href = canvas.toDataURL("image/png");
-        downloadLink.click();
-    })
+    document.body.removeAttribute("style");
+    this.svgBoard.container.removeAttribute("style");
   }
 
   downloadSVG() {
     const data = new XMLSerializer().serializeToString(this.svgBoard.container);
-    const a = document.createElement("a");
-    a.download = "image.svg";
-    a.href = "data:image/svg+xml;base64," + btoa(data);
-    a.click();
+    const blob = new Blob([data], { type: "image/svg+xml;charset=utf-8" });
+    const url = URL.createObjectURL(blob);
+
+    this.downloadResource("image.svg", url);
+  }
+
+  downloadResource(filename, href) {
+    const downloadLink = document.createElement("a");
+    downloadLink.download = filename;
+    downloadLink.href = href;
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
   }
 }
